049d9bc0dda4f23b5619a0e2c818a4c12cc1025a
박지수 <jisupark2000@gmail.com>
2024-08-14 09:53:13 +0900

    chore: Refactor contour_extraction.py for GPU acceleration and code organization

--- contour_extraction.py
+++ contour_extraction.py
@@ -13,45 +13,47 @@ import torch
 import matplotlib.pyplot as plt
 from PIL import Image
 
-device = torch.device("cuda")
-print(device)
 
-torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
-# turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
-if torch.cuda.get_device_properties(0).major >= 8:
-    torch.backends.cuda.matmul.allow_tf32 = True
-    torch.backends.cudnn.allow_tf32 = True
+def contour_extraction(image_file):
 
+    device = torch.device("cuda")
+    print(device)
 
-# Load the image
-image = Image.open("images/star.png")
-image = np.array(image.convert("RGB"))
+    torch.autocast("cuda", dtype=torch.bfloat16).__enter__()
+    # turn on tfloat32 for Ampere GPUs (https://pytorch.org/docs/stable/notes/cuda.html#tensorfloat-32-tf32-on-ampere-devices)
+    if torch.cuda.get_device_properties(0).major >= 8:
+        torch.backends.cuda.matmul.allow_tf32 = True
+        torch.backends.cudnn.allow_tf32 = True
 
-# Load the model
-from sam2.build_sam import build_sam2
-from sam2.sam2_image_predictor import SAM2ImagePredictor
+    # Load the image
+    image = Image.open(image_file)
+    image = np.array(image.convert("RGB"))
 
-sam2_checkpoint = "../checkpoints/sam2_hiera_large.pt"
-# sam2_checkpoint = "../checkpoints/sam2_hiera_tiny.pt"
-model_cfg = "sam2_hiera_l.yaml"
+    # Load the model
+    from sam2.build_sam import build_sam2
+    from sam2.sam2_image_predictor import SAM2ImagePredictor
 
-sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
+    sam2_checkpoint = "../checkpoints/sam2_hiera_large.pt"
+    # sam2_checkpoint = "../checkpoints/sam2_hiera_tiny.pt"
+    model_cfg = "sam2_hiera_l.yaml"
 
-predictor = SAM2ImagePredictor(sam2_model)
-predictor.set_image(image)
+    sam2_model = build_sam2(model_cfg, sam2_checkpoint, device=device)
 
-# Predict the mask
-input_point = np.array([[100, 100]])
-input_label = np.array([1])
+    predictor = SAM2ImagePredictor(sam2_model)
+    predictor.set_image(image)
 
-masks, scores, logits = predictor.predict(
-    point_coords=input_point,
-    point_labels=input_label,
-    multimask_output=False,
-)
+    # Predict the mask
+    input_point = np.array([[100, 100]])
+    input_label = np.array([1])
 
-# Convert the mask to contour
-result = contour(masks)
-c = normalize_coordinates(result)
-c = sorted_by_contour(c)
-c = change_to_map_scale(c)
+    masks, scores, logits = predictor.predict(
+        point_coords=input_point,
+        point_labels=input_label,
+        multimask_output=False,
+    )
+
+    # Convert the mask to contour
+    result = contour(masks)
+    c = normalize_coordinates(result)
+    c = sorted_by_contour(c)
+    c = change_to_map_scale(c)
